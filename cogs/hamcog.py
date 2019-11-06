90a456361f2f58bc4f591552424317e71cabb592
Abigail Gold <me@kb6.ee>
2019-11-06 00:37:35 -0500

    add command to display vanity callsign rules

--- cogs/hamcog.py
+++ cogs/hamcog.py
@@ -73,6 +73,65 @@ class HamCog(commands.Cog):
                              icon_url=str(ctx.author.avatar_url))
         await ctx.send(embed=embed)
 
+    @commands.command(name="vanities", aliases=["vanity", "pfx", "prefixes", "prefix"])
+    async def _vanity_prefixes(self, ctx: commands.Context):
+        '''Lists valid vanity prefixes for the US.'''
+        embed = discord.Embed(title='Valid US Vanity Prefixes',
+                              description=('#x# is the number of letters in the prefix and suffix of a callsign.'
+                                           'E.g., WY4RC would be a 2x2 callsign, with prefix WY and suffix RC.'),
+                              colour=self.gs.colours.good,
+                              timestamp=datetime.utcnow())
+        embed.set_footer(text=ctx.author.name,
+                         icon_url=str(ctx.author.avatar_url))
+
+        group_a = ('**Any:** K, N, W (1x2)\n'
+                   '    AA-AL, KA-KZ, NA-NZ, WA-WZ (2x1)\n'
+                   '    AA-AL (2x2)\n'
+                   '*Except*\n'
+                   '**Alaska:** AL, KL, NL, WL (2x1)\n'
+                   '**Caribbean:** KP, NP, WP (2x1)\n'
+                   '**Pacific:** AH, KH, NH, WH (2x1)')
+
+        embed.add_field(name='**Group A** (Extra Only)', value=group_a, inline=False)
+
+        group_b = ('**Any:** K, N, W (1x2)\n'
+                   '    AA-AL, KA-KZ, NA-NZ, WA-WZ (2x1)\n'
+                   '    AA-AL (2x2)\n'
+                   '*Except*\n'
+                   '**Alaska:** AL, KL, NL, WL (2x1)\n'
+                   '**Caribbean:** KP, NP, WP (2x1)\n'
+                   '**Pacific:** AH, KH, NH, WH (2x1)')
+
+        embed.add_field(name='**Group B** (Advanced and Extra Only)', value=group_b, inline=False)
+
+        group_c = ('**Any Region:** K, N, W (1x3)\n'
+                   '*Except*\n'
+                   '**Alaska:** KL, NL, WL (2x2)\n'
+                   '**Caribbean:** NP, WP (2x2)\n'
+                   '**Pacific:** KH, NH, WH (2x2)')
+
+        embed.add_field(name='**Group C** (Technician, General, Advanced, Extra Only)', value=group_c, inline=False)
+
+        group_d = ('**Any Region:** KA-KZ, WA-WZ (2x3)\n'
+                   '*Except*\n'
+                   '**Alaska:** KL, WL (2x3)\n'
+                   '**Caribbean:** KP, WP (2x3)\n'
+                   '**Pacific:** KH, WH (2x3)')
+
+        embed.add_field(name='**Group D** (Any License Class)', value=group_d, inline=False)
+
+        unavail = ('- KA2AA-KA9ZZ, KC4AAA-KC4AAF, KC4USA-KC4USZ, KG4AA-KG4ZZ, KC6AA-KC6ZZ, KL9KAA-KL9KHZ, KX6AA-KX6ZZ\n'
+                   '- Any suffix SOS or QRA-QUZ\n'
+                   '- Any 2x3 with X as the first suffix letter\n'
+                   '- Any 2x3 with AF, KF, NF, or WF prefix and suffix EMA\n'
+                   '- Any 2x3 with AA-AL, NA-NZ, WC, WK, WM, WR, or WT prefix\n'
+                   '- Any 2x1, 2x2, or 2x3 with KP, NP, WP prefix and 0, 6, 7, 8, 9 numeral\n'
+                   '- Any 1x1 callsign')
+
+        embed.add_field(name='**Unavailable**', value=unavail, inline=False)
+
+        await ctx.send(embed=embed)
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(HamCog(bot))
