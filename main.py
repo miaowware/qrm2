b25b5a95ffd473c75fbaa3fee16c863dd59fed34
Abigail <me@kb6.ee>
2019-12-23 14:06:42 -0500

    refactor to allow for fixed, random, or by time statuses

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ General Public License, version 2.
 """
 
 from datetime import time, datetime
+import random
 
 import discord
 from discord.ext import commands, tasks
@@ -115,6 +116,12 @@ async def _extctl_unload(ctx: commands.Context, extension: str):
 async def on_ready():
     print(f"Logged in as: {bot.user} - {bot.user.id}")
     print("------")
+    if opt.status_mode == "time":
+        _ensure_activity_time.start()
+    elif opt.status_mode == "random":
+        _ensure_activity_random.start()
+    else:
+        _ensure_activity_fixed.start()
 
 
 @bot.event
@@ -130,8 +137,8 @@ async def on_message(message):
 # --- Tasks ---
 
 @tasks.loop(minutes=5)
-async def _ensure_activity():
-    status = opt.status_default
+async def _ensure_activity_time():
+    status = opt.statuses[0]
 
     try:
         tz = pytz.timezone(opt.status_tz)
@@ -141,7 +148,7 @@ async def _ensure_activity():
 
     now = datetime.now(tz=tz).time()
 
-    for sts in opt.statuses:
+    for sts in opt.time_statuses:
         start_time = time(hour=sts[1][0], minute=sts[1][1], tzinfo=tz)
         end_time = time(hour=sts[2][0], minute=sts[2][1], tzinfo=tz)
         if start_time < now <= end_time:
@@ -149,18 +156,23 @@ async def _ensure_activity():
 
     await bot.change_presence(activity=discord.Game(name=status))
 
+@tasks.loop(minutes=5)
+async def _ensure_activity_random():
+    status = random.choice(opt.statuses)
 
-@_ensure_activity.before_loop
-async def _before_ensure_activity():
-    await bot.wait_until_ready()
+    await bot.change_presence(activity=discord.Game(name=status))
+
+@tasks.loop(minutes=5)
+async def _ensure_activity_fixed():
+    status = opt.statuses[0]
+
+    await bot.change_presence(activity=discord.Game(name=status))
 
 
 # --- Run ---
 
 for ext in opt.exts:
     bot.load_extension(ext_dir + '.' + ext)
-
-_ensure_activity.start()
 
 
 try:
