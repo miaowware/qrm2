ff5de8abc07b2ef77ce2ba2a2e4d7df79b429db7
thxo <thx@uw.edu>
2021-04-09 13:59:28 -0700

    replace error message with common mistakes

--- exts/tex.py
+++ exts/tex.py
@@ -45,7 +45,11 @@ class TexCog(commands.Cog):
                 if render_result["status"] != "success":
                     embed = cmn.embed_factory(ctx)
                     embed.title = "LaTeX Rendering Failed!"
-                    embed.description = render_result.get("description", "Unknown error")
+                    embed.description = ("Here are some common reasons:\n"
+                                         "- Did you forget to use math mode? Surround math expressions with `$`,"
+                                         " like `$x^3$`.\n"
+                                         "- Are you using a command from a package? It might not be available.\n"
+                                         "- Are you including the document headers? We already did that for you.")
                     embed.colour = cmn.colours.bad
                     await ctx.send(embed=embed)
                     return
